125a490534339b51cd61cdd1eaedc539969f6670
amasha26 <amashagunathilaka26@gmail.com>
2022-05-02 21:50:06 +0530

    initial commit

---
+++ app/src/main/java/com/example/testinginsurance/Case_Confirmation_Screen.kt
@@ -0,0 +1,11 @@
+package com.example.testinginsurance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Case_Confirmation_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_case_confirmation_screen)
+    }
+}
---
+++ app/src/main/java/com/example/testinginsurance/Previous_Case_Screen.kt
@@ -0,0 +1,11 @@
+package com.example.testinginsurance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Previous_Case_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_previous_case_screen)
+    }
+}

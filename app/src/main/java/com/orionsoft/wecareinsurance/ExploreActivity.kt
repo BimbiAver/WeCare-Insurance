e6aa50124d8b1695eb461e2b521deabd6e314367
Deshan D. Abeykoon <deshangoldcity@gmail.com>
2022-05-15 14:45:23 +0530

    Some files replaced. And colours and themes.xml has been updated

--- app/src/main/java/com/orionsoft/wecareinsurance/ExploreActivity.kt
+++ app/src/main/java/com/orionsoft/wecareinsurance/ExploreActivity.kt
@@ -1,11 +1,19 @@
 package com.orionsoft.wecareinsurance
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ExploreActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_explore)
+
+        val buttonClick = findViewById<Button>(R.id.btn_start)
+        buttonClick.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/orionsoft/wecareinsurance/SplashActivity.kt
+++ app/src/main/java/com/orionsoft/wecareinsurance/SplashActivity.kt
@@ -1,11 +1,24 @@
 package com.orionsoft.wecareinsurance
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
+
+    private var SPLASH_TIME: Long = 2000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.myLooper()!!).postDelayed ({
+            startActivity(Intent(this, ExploreActivity::class.java))
+            finish()
+        },SPLASH_TIME)
+        }
+
+
     }
-}

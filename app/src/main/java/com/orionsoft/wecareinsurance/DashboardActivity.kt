bf0b9a3a36843025a8abb9b07a83e0b0cc61ad3d
Deshan D. Abeykoon <deshangoldcity@gmail.com>
2022-05-17 10:43:22 +0530

    DashboardActivity is completed

--- app/src/main/java/com/orionsoft/wecareinsurance/DashboardActivity.kt
+++ app/src/main/java/com/orionsoft/wecareinsurance/DashboardActivity.kt
@@ -1,37 +1,124 @@
 package com.orionsoft.wecareinsurance
 
+import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
-class DashboardActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var txtDashboardGreet: TextView
+    private lateinit var btnLogOut: Button
+    private lateinit var cdViewReportCase: CardView
+    private lateinit var cdViewPrevCases: CardView
+    private lateinit var cdViewRegVehicles: CardView
+    private lateinit var cdViewContactUs: CardView
+
+    private var sharedPrefManager: SharedPrefManager? = null // SharedPreferences for login session
+
+//        -----------------------------------------------------------------------------------------------
+
+    //    Activity wide interface - onClick() method
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.btnLogOut -> {
+                logout() // User Logout
+            }
+            R.id.cdViewReportCase -> {
+                val intent = Intent(this, ReportVehicleActivity::class.java)
+                startActivity(intent)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            }
+            R.id.cdViewPrevCases -> {
+                val intent = Intent(this, PreviousCasesActivity::class.java)
+                startActivity(intent)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            }
+            R.id.cdViewRegVehicles -> {
+                val intent = Intent(this, RegisteredVehiclesActivity::class.java)
+                startActivity(intent)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            }
+            R.id.cdViewContactUs -> {
+                val intent = Intent(this, ContactActivity::class.java)
+                startActivity(intent)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            }
+        }
+    }
+
+//        -----------------------------------------------------------------------------------------------
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
-        val buttonClick = findViewById<ImageButton>(R.id.img_btn_report_case)
-        buttonClick.setOnClickListener{
-            val intent = Intent(this, ReportVehicleActivity::class.java)
-            startActivity(intent)
-        }
+        sharedPrefManager = SharedPrefManager(this) // Instantiate the SharedPrefManager
 
-        val buttonClick2 = findViewById<ImageButton>(R.id.img_btn_previous)
-        buttonClick2.setOnClickListener{
-            val intent = Intent(this, PreviousCasesActivity::class.java)
-            startActivity(intent)
-        }
-        val buttonClick3 = findViewById<ImageButton>(R.id.img_btn_vehicle)
-        buttonClick3.setOnClickListener{
-            val intent = Intent(this, RegisteredVehiclesActivity::class.java)
-            startActivity(intent)
-        }
-        val buttonClick4 = findViewById<ImageButton>(R.id.img_btn_contact)
-        buttonClick4.setOnClickListener{
-            val intent = Intent(this, ContactActivity::class.java)
-            startActivity(intent)
-        }
+        txtDashboardGreet = findViewById(R.id.txtDashboardGreet)
+        btnLogOut = findViewById(R.id.btnLogOut)
+        cdViewReportCase = findViewById(R.id.cdViewReportCase)
+        cdViewPrevCases = findViewById(R.id.cdViewPrevCases)
+        cdViewRegVehicles = findViewById(R.id.cdViewRegVehicles)
+        cdViewContactUs = findViewById(R.id.cdViewContactUs)
 
+        var greetName = sharedPrefManager!!.getPreferenceString("name")
+        txtDashboardGreet.text = "Hello, " + greetName!!.substring(greetName.lastIndexOf(" ")+1)
+
+        // Instantiate the setOnClickListener(s) at runtime
+        btnLogOut.setOnClickListener(this)
+        cdViewReportCase.setOnClickListener(this)
+        cdViewPrevCases.setOnClickListener(this)
+        cdViewRegVehicles.setOnClickListener(this)
+        cdViewContactUs.setOnClickListener(this)
+    }
+
+//        -----------------------------------------------------------------------------------------------
+
+    // When the BACK button is pressed, the activity on the stack is restarted
+    override fun onRestart() {
+        super.onRestart()
+        finish()
+        startActivity(intent)
+    }
+
+//        -----------------------------------------------------------------------------------------------
+
+    override fun onBackPressed() {
+        AlertDialog.Builder(this)
+            .setIcon(R.drawable.ic_exit)
+            .setTitle("Confirm Exit")
+            .setMessage("Are you sure you want to exit?")
+            .setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, which ->
+                finishAffinity() // Removes the connection of the existing activity to its stack
+                finish() // The method onDestroy() is executed & exit the application
+            })
+            .setNegativeButton("No", null).show()
+    }
+
+//        -----------------------------------------------------------------------------------------------
+
+    private fun logout() {
+        AlertDialog.Builder(this)
+            .setIcon(R.drawable.ic_exit)
+            .setTitle("Confirm Logout")
+            .setMessage("Are you sure you want to logout?")
+            .setPositiveButton("Yes") { dialog, which ->
+                sharedPrefManager!!.clearSharedPreference()
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                finishAffinity()
+                finish()
+            }
+            .setNegativeButton("No", null).show()
     }
 }
